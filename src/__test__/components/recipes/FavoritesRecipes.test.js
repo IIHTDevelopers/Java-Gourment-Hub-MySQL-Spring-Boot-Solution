64bd5d51e7ee5c958fff32d8d7d0c6df5f22204f
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-08-20 18:25:58 +0530

    Update FavoritesRecipes.test.js

--- src/__test__/components/recipes/FavoritesRecipes.test.js
+++ src/__test__/components/recipes/FavoritesRecipes.test.js
@@ -9,10 +9,6 @@ describe('boundary', () => {
         fileContent = fs.readFileSync(filePath, 'utf8');
     });
 
-    test('FavoriteRecipesComponent boundary should fetch payments made by the user', () => {
-        expect(fileContent).toMatch(/axios\.get\(`http:\/\/localhost:4000\/payments\?userId=\$\{user\.id\}`\)/);
-    });
-
     test('FavoriteRecipesComponent boundary should contain loading state', () => {
         expect(fileContent).toMatch(/const \[loading, setLoading\] = useState\(true\);/);
     });
