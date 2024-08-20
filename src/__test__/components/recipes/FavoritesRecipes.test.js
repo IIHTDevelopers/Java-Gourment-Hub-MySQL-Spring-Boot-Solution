e97735faccf223c1cf5135cec0824bd57ea4fa4b
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-08-20 18:19:10 +0530

    Update FavoritesRecipes.test.js

--- src/__test__/components/recipes/FavoritesRecipes.test.js
+++ src/__test__/components/recipes/FavoritesRecipes.test.js
@@ -9,16 +9,8 @@ describe('boundary', () => {
         fileContent = fs.readFileSync(filePath, 'utf8');
     });
 
-    test('FavoriteRecipesComponent boundary should fetch favorite recipes for the user', () => {
-        expect(fileContent).toMatch(/axios\.get\(`http:\/\/localhost:4000\/favorites\?userId=\$\{user\.id\}`\)/);
-    });
-
     test('FavoriteRecipesComponent boundary should fetch payments made by the user', () => {
         expect(fileContent).toMatch(/axios\.get\(`http:\/\/localhost:4000\/payments\?userId=\$\{user\.id\}`\)/);
-    });
-
-    test('FavoriteRecipesComponent boundary should fetch all recipes', () => {
-        expect(fileContent).toMatch(/axios\.get\('http:\/\/localhost:4000\/recipes'\)/);
     });
 
     test('FavoriteRecipesComponent boundary should contain loading state', () => {
@@ -27,10 +19,6 @@ describe('boundary', () => {
 
     test('FavoriteRecipesComponent boundary should contain payment success handler', () => {
         expect(fileContent).toMatch(/handlePaymentSuccess/);
-    });
-
-    test('FavoriteRecipesComponent boundary should display RecipeDetail for paid recipes', () => {
-        expect(fileContent).toMatch(/paidRecipeIds\.includes\(recipe\.id\) \? \(\s*<RecipeDetail recipe=\{recipe\} \/>/);
     });
 
     test('FavoriteRecipesComponent boundary should display PaymentForm for unpaid recipes', () => {
